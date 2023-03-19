326c36f3abe8161cb7d46f55f27069247263ddff
smirnypavel <89521405+smirnypavel@users.noreply.github.com>
2023-03-19 10:25:58 +0000

    Deploying to gh-pages from @ smirnypavel/goit-react-hw-05-movies@2674ae7aa02261d288bbccd25dd895991420dc92 🚀

--- static/js/691.28b5f14b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[691],{691:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184),c=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
-//# sourceMappingURL=691.28b5f14b.chunk.js.map
